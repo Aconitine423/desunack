dd89558547571ef9085575156768deffb29bfc8e
Aconitine423 <insu423@gmail.com>
2025-08-06 11:38:26 +0900

    start

---
+++ src/test/java/com/desunack/desunack/DesunackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desunack.desunack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesunackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

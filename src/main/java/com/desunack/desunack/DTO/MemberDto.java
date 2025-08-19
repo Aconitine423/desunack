20ee7d192716538d756c6e731e7d83b585717f3d
che <silverperiod112@gmail.com>
2025-08-19 11:06:06 +0900

    멤버DTO

---
+++ src/main/java/com/desunack/desunack/DTO/MemberDto.java
@@ -0,0 +1,32 @@
+package com.desunack.desunack.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.time.LocalDate;
+
+@Data
+@Accessors(chain=true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MemberDto {
+    private int memberUid;
+    private String memberId;
+    private String memberName;
+    private String memberPw;
+    private String memberEmail;
+    private String memberPost;
+    private String memberAddress;
+    private String memberAddressDetail;
+    private char memberKind;
+    private char memberStatus;
+    private LocalDate signupDate;
+    private LocalDate recentDate;
+
+
+}
+

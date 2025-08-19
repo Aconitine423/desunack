1fffcc664a4b606099807ea7000cb69a41769624
che <silverperiod112@gmail.com>
2025-08-19 11:21:20 +0900

    input태그 추가

---
+++ src/main/java/com/desunack/desunack/memberController.java
@@ -0,0 +1,16 @@
+package com.desunack.desunack;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping
+public class memberController {
+    @GetMapping("/login1")
+    public String login(){
+        return "member/login";
+    }
+}

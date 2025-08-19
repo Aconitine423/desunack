9ef95eae42c603c52983717afd6c6f7e3a43f605
Aconitine423 <insu423@gmail.com>
2025-08-19 10:03:36 +0900

    시큐리티 경로 추가 및 컨트롤러로 경로 변경

---
+++ src/main/java/com/desunack/security/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.desunack.security.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class HomeController {
+    @GetMapping("/signup")
+    public String signup(){
+        return "/signup/signup";
+    }
+    @GetMapping("/signup/customerfrm")
+    public String customerJoin(){
+        return "/signup/customerfrm";
+    }
+}

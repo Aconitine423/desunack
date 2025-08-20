3f4d85d11d04220ef3c6e296731362171dc432f5
che <silverperiod112@gmail.com>
2025-08-20 16:19:30 +0900

    login메소드

--- src/main/java/com/desunack/desunack/DAO/MemberDao.java
+++ src/main/java/com/desunack/desunack/DAO/MemberDao.java
@@ -1,5 +1,6 @@
 package com.desunack.desunack.DAO;
 
+import com.desunack.desunack.Entity.Member;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -8,4 +9,6 @@ public interface MemberDao {
 
     @Select("select m_pw from member where m_id = #{m_id}")
     String getSecurityPw(String m_id);
+
+    Member getMemberEntity(String userId);
 }
--- src/main/java/com/desunack/desunack/service/MemberService.java
+++ src/main/java/com/desunack/desunack/service/MemberService.java
@@ -2,8 +2,11 @@ package com.desunack.desunack.service;
 
 
 import com.desunack.desunack.DAO.MemberDao;
+import com.desunack.desunack.DTO.UserDto;
+import com.desunack.desunack.Entity.Member;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -11,9 +14,21 @@ import org.springframework.stereotype.Service;
 public class MemberService {
     @Autowired
     private MemberDao mDao;
+    private UserDto uDto;
+    private Member member;
 
-
-    public boolean login1(){
-        return false;
+    public UserDto login1(String id, String pw){
+        String ecdpw = mDao.getSecurityPw(id);
+        if(ecdpw != null){
+            log.info("========id ok========");
+            BCryptPasswordEncoder ecd = new BCryptPasswordEncoder();
+            if(ecd.matches(pw,ecdpw)){
+                log.info("========success=======");
+                member = mDao.getMemberEntity(id);
+                uDto = member.toDto();
+                return uDto;
+            }
+        }
+        return null;
     }
 }

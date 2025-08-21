1199fb57864ef0ff856ecc7d9a40eccb4b417148
che <silverperiod112@gmail.com>
2025-08-21 12:21:24 +0900

    로그인 최종 커밋

--- src/main/java/com/desunack/desunack/service/MemberService.java
+++ src/main/java/com/desunack/desunack/service/MemberService.java
@@ -2,8 +2,13 @@ package com.desunack.desunack.service;
 
 
 import com.desunack.desunack.DAO.MemberDao;
+import com.desunack.desunack.DTO.CustomerDto;
+import com.desunack.desunack.DTO.SellerDto;
 import com.desunack.desunack.DTO.UserDto;
+import com.desunack.desunack.Entity.CustomerEntity;
 import com.desunack.desunack.Entity.MemberEntity;
+import com.desunack.desunack.Entity.SellerEntity;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,7 +22,7 @@ public class MemberService {
     private UserDto uDto;
     private MemberEntity memberEntity;
 
-    public UserDto login1(String id, String pw){
+    public boolean login1(String id, String pw, HttpSession session){
         String ecdpw = mDao.getSecurityPw(id);
         if(ecdpw != null){
             log.info("========id ok========");
@@ -25,11 +30,26 @@ public class MemberService {
             if(ecd.matches(pw,ecdpw)){
                 log.info("========success=======");
                 memberEntity = mDao.getMemberEntity(id);
+                switch(memberEntity.getM_kind()){
+                    case 'A':
+                        session.setAttribute("member", memberEntity);
 
-//                uDto = member.toDto();
-                return uDto;
+                        break;
+                    case 'C':
+                        CustomerEntity cEntity = mDao.getCustomerEntity(memberEntity.getM_uid());
+                        CustomerDto cDto = cEntity.toDto(memberEntity);
+                        session.setAttribute("member", cDto);
+                        break;
+                    case 'S':
+                        SellerEntity sEntity = mDao.getSellerEntity(memberEntity.getM_uid());
+                        SellerDto sDto = sEntity.toDto(memberEntity);
+                        session.setAttribute("member", sDto);
+                        break;
+                }
+                return true;
             }
+            return false;
         }
-        return null;
+        return false;
     }
 }

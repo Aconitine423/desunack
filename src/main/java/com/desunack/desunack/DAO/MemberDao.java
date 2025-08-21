896d602ae4f715a4a036dd420f04a648701afef5
che <silverperiod112@gmail.com>
2025-08-21 15:44:30 +0900

    id/pw찾기

--- src/main/java/com/desunack/desunack/DAO/MemberDao.java
+++ src/main/java/com/desunack/desunack/DAO/MemberDao.java
@@ -12,12 +12,20 @@ public interface MemberDao {
     @Select("select m_pw from member where m_id = #{m_id}")
     String getSecurityPw(String m_id);
 
-    MemberEntity getMemberEntity(String userId);
+    MemberEntity getMemberEntity(String m_id);
 
 
-    CustomerEntity getCustomerEntity(int m_Uid);
+    CustomerEntity getCustomerEntity(int m_uid);
+
+    SellerEntity getSellerEntity(int m_uid);
 
     SellerEntity getSellerEntity(int mUid);
+
+    String findId(String m_name, String m_email);
+
+    int findPw(String m_id, String m_email);
+
+    boolean updatePw(String m_id, String ecdpw);
 
     void memberJoin(CustomerEntity customerEntity);
 
@@ -29,3 +37,4 @@ public interface MemberDao {
     @Select("select count(*) from member where m_id = #{mId}")
     boolean isUsedId(String mId);
 }
+

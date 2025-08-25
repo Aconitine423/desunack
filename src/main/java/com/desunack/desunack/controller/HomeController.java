1316c297199f769d53725d84accdd9c9b09e1b75
che <silverperiod112@gmail.com>
2025-08-25 14:15:02 +0900

    Json 세션에 저장, 프론트에서 동적 HTML 생성 필요

--- src/main/java/com/desunack/desunack/controller/HomeController.java
+++ src/main/java/com/desunack/desunack/controller/HomeController.java
@@ -1,24 +1,46 @@
 package com.desunack.desunack.controller;
 
+import com.desunack.desunack.DAO.MemberDao;
+import com.desunack.desunack.service.MemberService;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.time.LocalDate;
 
 @Slf4j
 @Controller
 public class HomeController {
-    @GetMapping("/")
-    public String home() {
-        return "index";
-    }
-
+    public MemberService mSer;
     @GetMapping("/signup")
     public String signup() {
         return "/signup/signup";
     }
-
     @GetMapping("/signup/customerfrm")
     public String customerJoin() {
         return "/signup/customerfrm";
     }
+
+    @GetMapping("/")
+    public String home(@RequestParam HttpSession session) {
+        char kind = (char) session.getAttribute("m_kind");
+        char gender = (char) session.getAttribute("m_gender");
+        int age =  (LocalDate.now().getYear() - (int) session.getAttribute("m_birth"))/10 * 10; // << 현재 연도와 태어난 연도의 차를 통해서 계산하기
+        if(kind == 'C'){
+            if(mSer.getGoodsSales(gender, age, session)){
+                return "/";
+            }
+        }else if(kind == 'S'){
+            int id = (int) session.getAttribute("m_id");
+            if(mSer.getCompanySales(id, session)){
+                return "/";
+            }
+        }
+        return "index";
+    }
+
 }
--- src/main/java/com/desunack/desunack/service/MemberService.java
+++ src/main/java/com/desunack/desunack/service/MemberService.java
@@ -14,7 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
 @Service
@@ -84,4 +84,78 @@ public class MemberService {
         return false;
     }
 
+    public boolean findId(String m_name, String m_email, RedirectAttributes rttr) {
+        String id = mDao.findId(m_name, m_email);
+        if(id != null){
+            String msg = m_name + "님의 ID는 " + id + "입니다";
+            rttr.addFlashAttribute("msg", msg);
+            return true;
+        }
+        return false;
+    }
+
+
+    public boolean findPw(String m_id, String m_email, RedirectAttributes rttr) {
+        int isExist = mDao.findPw(m_id, m_email);
+        if(isExist != 0){
+            StringBuilder sb = new StringBuilder();
+            int randNum;
+            char randChar;
+            for(int i = 0; i < 12; i++){
+                randNum = (int)(Math.random() * 1000);
+                randNum = randNum % 62;
+                if(randNum > 9 && randNum < 36){
+                    randChar = (char) (randNum + 87);
+                    sb.append(randChar);
+                }else if(randNum > 35){
+                    randChar = (char) (randNum + 28);
+                    sb.append(randChar);
+                }
+                else{
+                    sb.append(randNum);
+                }
+            }
+            BCryptPasswordEncoder ecd = new BCryptPasswordEncoder();
+            String pw = sb.toString();
+            String ecdpw = ecd.encode(pw);
+            if(mDao.updatePw(m_id, ecdpw)){
+                String msg = m_id + "님의 임시 비밀번호는 " + pw + "입니다";
+                rttr.addFlashAttribute("msg", msg);
+            }else{
+                String msg = "서버와의 연결이 원활하지 않습니다.";
+                rttr.addFlashAttribute("msg", msg);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public boolean getGoodsSales(char m_gender, int m_age, HttpSession session){
+        //m_age와 m_gender를 통해 상품-판매량 조인해서 검색결과 반환(JSON)한 후
+        //JSON을 분해해 나온 goodsDto에 저장된 데이터를 String Builder에 저장해서 출력시킬내용 세션에 저장하기
+        //조인해서 검색 결과 반환하는 쿼리문
+        //select * from 상품 join 상품판매
+        // on 상품ID = 상품판매ID and 상품판매성별 = 회원성별 and 상품판매나이 = 회원나이대
+        //order by 상품판매량 desc;
+        String Json = mDao.getGoodsSales(m_gender, m_age);
+        if(Json != null){
+            session.setAttribute("Json", Json);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean getCompanySales(int m_uid, HttpSession session){
+        //m_uid를 통해 상품판매량 조인해서 검색결과 반환한 후
+        //goodsDto에 저장된 데이터를 String Builder에 저장해서 출력시킬내용 세션에저장하기
+        //쿼리문
+        //select * from 상품 join 전체판매 on 상품ID = 상품판매ID
+        //where 판매자ID = m_id order by 상품판매량 desc
+        String Json = mDao.getCompanySales(m_uid);
+        if(Json != null){
+            session.setAttribute("Json", Json);
+            return true;
+        }
+        return false;
+    }
 }

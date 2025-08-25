3c5a72205d2eab6d8dfe71a2c0ad189352ee6250
che <silverperiod112@gmail.com>
2025-08-25 11:28:40 +0900

    소비자/판매자 회원 로그인시 보이는 정렬 sql문 작성
    (cherry picked from commit 30ca649ba0b2666bab9cf064c26a80946f4dfa4d)

--- src/main/java/com/desunack/desunack/DAO/MemberDao.java
+++ src/main/java/com/desunack/desunack/DAO/MemberDao.java
@@ -12,12 +12,16 @@ public interface MemberDao {
     @Select("select m_pw from member where m_id = #{m_id}")
     String getSecurityPw(String m_id);
 
-    MemberEntity getMemberEntity(String userId);
+    MemberEntity getMemberEntity(String m_id);
 
 
-    CustomerEntity getCustomerEntity(int m_Uid);
+    CustomerEntity getCustomerEntity(int m_uid);
 
-    SellerEntity getSellerEntity(int mUid);
+    SellerEntity getSellerEntity(int m_uid);
+
+    String findId(String m_name, String m_email);
+
+    int findPw(String m_id, String m_email);
 
     void memberJoin(CustomerEntity customerEntity);
 
@@ -28,4 +32,10 @@ public interface MemberDao {
 
     @Select("select count(*) from member where m_id = #{mId}")
     boolean isUsedId(String mId);
+
+    boolean updatePw(String m_id, String ecdpw);
+
+    String getGoodsSales(char m_gender, int m_age);
+
+    String getCompanySales(int m_uid);
 }

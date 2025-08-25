cabf1e93846e5ffec10760170fc68abb4f587d0e
Aconitine423 <insu423@gmail.com>
2025-08-25 15:30:49 +0900

    userKind값 A에서 C로 변경

--- src/main/java/com/desunack/desunack/service/MemberService.java
+++ src/main/java/com/desunack/desunack/service/MemberService.java
@@ -14,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j

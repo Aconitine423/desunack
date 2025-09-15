2d9adb4b5de65c053ef462b126e7be6a8320510c
Aconitine423 <insu423@gmail.com>
2025-09-15 12:12:07 +0900

    파일업로드 경로 수정 및 파일 수동 추가

--- src/main/java/com/desunack/desunack/config/webMvcConfig.java
+++ src/main/java/com/desunack/desunack/config/webMvcConfig.java
@@ -8,15 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class webMvcConfig implements WebMvcConfigurer {
 
-    @Value("${file.upload.path}")
-    private String uploadPath;
+//    @Value("${file.upload.path}")
+//    private String uploadPath;
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // '/uploads/**'로 시작하는 모든 요청을
-        // 'file:///C:/dev/upload/' 경로에 있는 파일로 연결합니다.
-        // 반드시 'file:///'을 붙여줘야 합니다.
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:///" + uploadPath);
-    }
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        // '/uploads/**'로 시작하는 모든 요청을
+//        // 'file:///C:/dev/upload/' 경로에 있는 파일로 연결합니다.
+//        // 반드시 'file:///'을 붙여줘야 합니다.
+////        registry.addResourceHandler("/uploads/**")
+////                .addResourceLocations("file:///" + uploadPath);
+//    }
 }

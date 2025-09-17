314323bfb20551e446f18b21dedcf2422ac33134
che <silverperiod112@gmail.com>
2025-09-17 15:05:04 +0900

    주문 기능 - 쿼리문 추가 작성

--- src/main/java/com/desunack/desunack/controller/OrderController.java
+++ src/main/java/com/desunack/desunack/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.desunack.desunack.controller;
 
+import com.desunack.desunack.dto.OrderDto;
 import com.desunack.desunack.dto.ShoppingCartDto;
 import com.desunack.desunack.service.OrderService;
 import jakarta.servlet.http.HttpSession;
@@ -8,10 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,6 +31,18 @@ public class OrderController {
             log.error("페이지 이동중 에러 발생", e);
             return ResponseEntity.badRequest().body("페이지 이동 실패");
 
+        }
+    }
+
+    @PostMapping("/goods/orderUpdate")
+    public ResponseEntity<String> goodsOrderUpdate(@RequestPart List<ShoppingCartDto> scList, @RequestPart OrderDto oDto, Model model){
+        try{
+            oSer.orderConfirm(scList, oDto, model);
+            return ResponseEntity.ok("주문 성공");
+        }
+        catch(Exception e){
+            log.error("주문 처리 중 에러 발생", e);
+            return ResponseEntity.badRequest().body("주문 실패");
         }
     }
 

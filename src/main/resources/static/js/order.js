44861943ae6222abcc780b93123890d05c59e4db
che <silverperiod112@gmail.com>
2025-09-18 17:29:20 +0900

    주문 기능 - 주문 정보 표시 추가

--- src/main/resources/static/js/order.js
+++ src/main/resources/static/js/order.js
@@ -1,24 +1,11 @@
-$('#same').on('click', function(){
-    console.log("3");
-    if($(this).is(":checked")){
-        console.log("1");
-        $('#recvName').val($('#buyerName').val());
-        $('#recvPhone1').val($('#buyerPhone1').val());
-        $('#recvPhone2').val($('#buyerPhone2').val());
-        $('#recvPhone3').val($('#buyerPhone3').val());
-        $('#recvPost').val($('#buyerPost').val());
-        $('#recvAddr').val($('#buyerAddr').val());
-        $('#recvDetail').val($('#buyerDetail').val());
-    }else{
-        console.log("2");
-        $('#recvName').val('');
-        $('#recvPhone1').val('');
-        $('#recvPhone2').val('');
-        $('#recvPhone3').val('');
-        $('#recvPost').val('');
-        $('#recvAddr').val('');
-        $('#recvDetail').val('');
-    }
+$('#same').change(function(){
+$('#recvName').val($('#buyerName').val());
+$('#recvPhone1').val($('#buyerPhone1').val());
+$('#recvPhone2').val($('#buyerPhone2').val());
+$('#recvPhone3').val($('#buyerPhone3').val());
+$('#recvPost').val($('#buyerPost').val());
+$('#recvAddr').val($('#buyerAddr').val());
+$('#recvDetail').val($('#buyerDetail').val());
 })
 
 function buyConfirm(){
@@ -26,12 +13,10 @@ function buyConfirm(){
     const idList = [];
 
     idbox.each(function(){
-        idList.push($(this).val())
+        idList.push($(this).value())
     });
 
     const OrderEntity = {
-        go_num : $(`#order${orderNum}`).val(),
-        go_m_uid : $(`#uid`).val(),
         go_receiver_name : $('#recvName').val(),
         go_receiver_phone : $('#recvPhone1').val() + '-' + $('#recvPhone2').val() + '-' + $('#recvPhone3').val(),
         go_receiver_post : $('#recvPost').val(),
@@ -41,7 +26,7 @@ function buyConfirm(){
         goc_card_com : $('#cardCompany').val(),
         goc_card_installment : $('#installment').val(),
         go_kind : 2,
-        gop_pay_type : 1,
+        gop_pay_type : "선불",
         gop_cost : 3000
 
     }
@@ -50,14 +35,11 @@ function buyConfirm(){
     formData.append('idList', new Blob([JSON.stringify(idList)], {type: 'application/json'}));
     formData.append('OrderEntity', new Blob([JSON.stringify(OrderEntity)], {type: 'application/json'}));
 
-    console.log(OrderEntity);
-    console.log(idList);
-    axios.post('/order/orderUpdate',formData).then(function(response){
+    axios.post('order/orderUpdate',formData).then(function(response){
         console.log("주문 성공", response.data);
         location.href = "/";
     })
         .catch(function(error){
             console.log(error);
         })
-
 }

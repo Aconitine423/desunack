7839a06c427ce16d29ac2e09b6974d3205d91f25
che <silverperiod112@gmail.com>
2025-09-19 14:32:08 +0900

    주문 기능 완성

--- src/main/java/com/desunack/desunack/controller/OrderController.java
+++ src/main/java/com/desunack/desunack/controller/OrderController.java
@@ -48,7 +48,7 @@ public class OrderController {
     }
 
     @GetMapping("/order/order/{go_num}")
-    public String goodsOrder(@PathVariable("go_num")int go_num, Model model){
+    public String goodsOrder(@PathVariable("go_num")int go_num, Model model, HttpSession session){
         oSer.loadOrder(go_num, model);
         return "/order/order";
     }
--- src/main/resources/static/js/order.js
+++ src/main/resources/static/js/order.js
@@ -1,11 +1,24 @@
-$('#same').change(function(){
-$('#recvName').val($('#buyerName').val());
-$('#recvPhone1').val($('#buyerPhone1').val());
-$('#recvPhone2').val($('#buyerPhone2').val());
-$('#recvPhone3').val($('#buyerPhone3').val());
-$('#recvPost').val($('#buyerPost').val());
-$('#recvAddr').val($('#buyerAddr').val());
-$('#recvDetail').val($('#buyerDetail').val());
+$('#same').on('click', function(){
+    console.log("3");
+    if($(this).is(":checked")){
+        console.log("1");
+        $('#recvName').val($('#buyerName').val());
+        $('#recvPhone1').val($('#buyerPhone1').val());
+        $('#recvPhone2').val($('#buyerPhone2').val());
+        $('#recvPhone3').val($('#buyerPhone3').val());
+        $('#recvPost').val($('#buyerPost').val());
+        $('#recvAddr').val($('#buyerAddr').val());
+        $('#recvDetail').val($('#buyerDetail').val());
+    }else{
+        console.log("2");
+        $('#recvName').val('');
+        $('#recvPhone1').val('');
+        $('#recvPhone2').val('');
+        $('#recvPhone3').val('');
+        $('#recvPost').val('');
+        $('#recvAddr').val('');
+        $('#recvDetail').val('');
+    }
 })
 
 function buyConfirm(){
@@ -13,10 +26,12 @@ function buyConfirm(){
     const idList = [];
 
     idbox.each(function(){
-        idList.push($(this).value())
+        idList.push($(this).val())
     });
 
     const OrderEntity = {
+        go_num : $(`#order${orderNum}`).val(),
+        go_m_uid : $(`#uid`).val(),
         go_receiver_name : $('#recvName').val(),
         go_receiver_phone : $('#recvPhone1').val() + '-' + $('#recvPhone2').val() + '-' + $('#recvPhone3').val(),
         go_receiver_post : $('#recvPost').val(),
@@ -26,7 +41,7 @@ function buyConfirm(){
         goc_card_com : $('#cardCompany').val(),
         goc_card_installment : $('#installment').val(),
         go_kind : 2,
-        gop_pay_type : "선불",
+        gop_pay_type : 1,
         gop_cost : 3000
 
     }
@@ -35,11 +50,14 @@ function buyConfirm(){
     formData.append('idList', new Blob([JSON.stringify(idList)], {type: 'application/json'}));
     formData.append('OrderEntity', new Blob([JSON.stringify(OrderEntity)], {type: 'application/json'}));
 
-    axios.post('order/orderUpdate',formData).then(function(response){
+    console.log(OrderEntity);
+    console.log(idList);
+    axios.post('/order/orderUpdate',formData).then(function(response){
         console.log("주문 성공", response.data);
         location.href = "/";
     })
         .catch(function(error){
             console.log(error);
         })
+
 }
